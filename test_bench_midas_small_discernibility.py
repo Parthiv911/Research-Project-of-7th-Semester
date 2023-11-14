b611a554e2ee5bde55a24de4becc9507360a420c
Parthiv Aneesh <101570923+Parthiv911@users.noreply.github.com>
2023-11-14 18:28:02 +0530

    Add files via upload

---
+++ test_bench_midas_small_discernibility.py
@@ -0,0 +1,131 @@
+import cv2
+import cv2
+import numpy as np
+import joblib
+import pickle
+from keras.models import Sequential
+from keras.models import load_model
+import torch
+import urllib.request
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+from utility import calculate_centroid, Solution
+import sys
+#sys.setrecursionlimit(1000)
+#Uncomment when using Windows if CuDNN does not work
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+#model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+
+def image_to_depth(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    input_batch = transform(img).to(device)
+
+    with torch.no_grad():
+        prediction = midas(input_batch)
+
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+    output = prediction.cpu().numpy()
+    return output
+
+
+
+
+
+
+resolutions=[100,150,200,250,300,350,400]
+areaOfInterest_thresholds=[0,10,20,30,40,50,60,70,80,90]
+discernability_results=np.zeros(shape=[len(resolutions),len(areaOfInterest_thresholds)])
+
+num_of_frames_limit=300
+
+for resolutions_index in range(len(resolutions)):
+    for areaOfInterest_thresholds_index in range(len(areaOfInterest_thresholds)):
+
+        num_of_frames = 0
+        predicted_count = 0
+        unable_to_predict_count = 0
+        cap = cv2.VideoCapture('CS_thirdfloor_leftwall.mp4', apiPreference=cv2.CAP_MSMF)
+
+        while num_of_frames < num_of_frames_limit:
+            ret, frame = cap.read()
+
+            if ret == False:
+                break
+            if ret == True:
+                if num_of_frames % 1 != 0 or num_of_frames < 200:
+                    num_of_frames = num_of_frames + 1
+                    continue
+                num_of_frames = num_of_frames + 1
+                print(num_of_frames)
+                frame = cv2.resize(frame, (resolutions[resolutions_index], resolutions[resolutions_index]))
+                depth_image = image_to_depth(frame)
+
+                solution1 = Solution(depth_image, areaOfInterest_thresholds[areaOfInterest_thresholds_index])
+                spaces_identified = solution1.identify_spaces_iterative()
+
+                if len(spaces_identified) == 0:
+                    unable_to_predict_count = unable_to_predict_count + 1
+                    centre_of_frame=[resolutions[resolutions_index],resolutions[resolutions_index]]
+                    cv2.rectangle(frame, (50 + 10, 50 + 10), (50 + 15, 50 + 15),(0, 255, 0), 1)
+                else:
+                    predicted_count = predicted_count + 1
+
+                for i in range(len(spaces_identified)):
+                    centroid = calculate_centroid(spaces_identified[i])
+                    centroid = np.rint(centroid).astype(int)
+
+                    cv2.rectangle(frame, (centroid[1] - 5, centroid[0] - 5), (centroid[1] + 5, centroid[0] + 5),(0, 0, 255), 1)
+
+                dist = frame
+                dist = dist.astype(np.uint8)
+                frame = dist
+                cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('video', 600, 600)
+                cv2.imshow("video", frame)
+
+                # videoFrames = np.concatenate((videoFrames, [frame]), axis=0)
+                # print(videoFrames.shape)
+
+                if (cv2.waitKey(10) & 0xFF == ord('q')):
+                    break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        predicted_ratio = predicted_count / (predicted_count + unable_to_predict_count)
+        discernability_results[resolutions_index][areaOfInterest_thresholds_index]=predicted_ratio
+        print(discernability_results)
+
+print(discernability_results)
+
+df = pd.DataFrame(discernability_results)
+writer = pd.ExcelWriter('discernability_results.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='welcome', index=False)
+writer.save()
+
+
+
---
+++ utility2.py
@@ -0,0 +1,147 @@
+import numpy as np
+import cv2
+import joblib
+import pickle
+from keras.models import Sequential
+from keras.models import load_model
+import torch
+import urllib.request
+class Solution:
+    def __init__(self,depth_map,areaOfInterest_threshold,forward_threshold):
+        self.theta_forward=forward_threshold
+        self.grid=np.copy(depth_map)
+        self.space_blobs = {}
+        self.count = 0
+        self.minimum = np.min(depth_map)
+        self.areaOfInterest_threshold=areaOfInterest_threshold
+        self.inside=False
+
+    def isAreaofInterest(self,i,j):
+        if(self.grid[i][j] <= self.minimum+self.areaOfInterest_threshold):
+            return True
+        return False
+
+    def dfs(self,i, j):
+        if i < 0 or j < 0 or i >= len(self.grid) or j >= len(self.grid[0]) or self.grid[i][j] == -1 or (not self.isAreaofInterest(i,j)):
+            return
+        self.grid[i][j] = 10000
+
+        self.space_blobs[self.count] = np.concatenate((self.space_blobs[self.count], [[i, j]]))
+
+        self.dfs(i + 1, j)
+        self.dfs(i - 1, j)
+        self.dfs(i, j + 1)
+        self.dfs(i, j - 1)
+
+    def identify_spaces(self):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                if self.isAreaofInterest(i,j):
+                    self.space_blobs[self.count] = np.empty([0, 2])
+                    self.dfs(i, j)
+                    self.count = self.count + 1
+        return self.space_blobs
+
+    def identify_spaces_iterative(self):
+
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                if self.isAreaofInterest(i,j):
+                    #self.space_blobs[self.count] = np.empty([0, 2])
+                    self.space_blobs[self.count] = []
+                    stack=[]
+                    stack.append([i,j])
+
+                    while(len(stack)!=0):
+                        cood_x=stack[-1][0]
+                        cood_y=stack[-1][1]
+                        #print(stack)
+                        stack.pop(len(stack)-1)
+
+                        if cood_x < 0 or cood_y < 0 or cood_x >= len(self.grid) or cood_y >= len(self.grid[0]) or self.grid[cood_x][cood_y] == -1 or (not self.isAreaofInterest(cood_x, cood_y)):
+                            #print(cood_x,cood_y)
+                            continue
+                        self.grid[cood_x][cood_y] = 100000
+                        #self.space_blobs[self.count] = np.concatenate((self.space_blobs[self.count], [[cood_x, cood_y]]))
+                        self.space_blobs[self.count].append([cood_x, cood_y])
+                        stack.append([cood_x + 1, cood_y])
+                        stack.append([cood_x - 1, cood_y])
+                        stack.append([cood_x, cood_y+1])
+                        stack.append([cood_x, cood_y-1])
+
+                    self.count = self.count + 1
+        return self.space_blobs
+    def decide_action(self):
+        n=len(self.grid[0])
+        centre=n/2
+
+        spaces_identified = self.identify_spaces_iterative()
+        if len(spaces_identified)==0:
+            return [-1,-1],"unable to decide"
+
+        centroids=[]
+        for i in range(len(spaces_identified)):
+            centroid = calculate_centroid(spaces_identified[i])
+            centroid = np.rint(centroid).astype(int)
+
+            centroids.append(centroid)
+
+        x_dist=1000000000
+        centroid_final=[-1,-1]
+        for i in range(len(centroids)):
+            '''
+            if np.abs(centroids[i][1]-centre)<x_dist:
+                centroid_final=centroids[i]
+                x_dist=np.abs(centroids[i][1]-centre)
+            '''
+            if (centroids[i][1] - centre)**2+(centroids[i][0] - centre)**2 < x_dist:
+                centroid_final = centroids[i]
+                x_dist = (centroids[i][1] - centre)**2+(centroids[i][0] - centre)**2
+
+
+        for i in range(len(centroids)):
+            #error possible
+            if centroid_final[1]<centre+self.theta_forward and centroid_final[1]>centre-self.theta_forward:
+                return centroid_final,"go forward"
+            elif centroid_final[1]>centre+self.theta_forward:
+                return centroid_final,"turn right"
+            else:
+                return centroid_final,"turn left"
+
+        return "error"
+
+
+
+def calculate_centroid(coordinates):
+    if len(coordinates)==0:
+        return (-1,-1)
+
+    centroid_x=0
+    centroid_y=0
+
+    for i in range(len(coordinates)):
+        centroid_x=centroid_x+coordinates[i][0]
+        centroid_y=centroid_y+coordinates[i][1]
+
+    centroid_x=centroid_x/len(coordinates)
+    centroid_y=centroid_y/len(coordinates)
+
+    return (centroid_x,centroid_y)
+
+'''
+def identify_space(depth_map):
+    standard_deviation=np.std(depth_map)
+    mean=np.mean(depth_map)
+    minimum=np.min(depth_map)
+    print("Mean: ",mean)
+    print("Standard Deviation: ",standard_deviation)
+    space_coordinates=np.empty(shape=[0,2])
+    #print(space_coordinates)
+    for i in range(len(depth_map)):
+        for j in range(len(depth_map[0])):
+            if(depth_map[i][j]<minimum+20):
+                cood=np.array([i,j])
+                space_coordinates=np.concatenate((space_coordinates,[cood]))
+
+    return space_coordinates
+'''
